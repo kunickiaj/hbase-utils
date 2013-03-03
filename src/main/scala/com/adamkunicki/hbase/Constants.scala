bdc014d545de905ad1357c998bdd6781a31016c6
Adam Kunicki <akunicki@42six.com>
2013-03-03 13:30:58 -0500

    Corrected typo in constants.

--- src/main/scala/com/adamkunicki/hbase/Constants.scala
+++ src/main/scala/com/adamkunicki/hbase/Constants.scala
@@ -1,7 +1,7 @@
 package com.adamkunicki.hbase
 
 object Constants {
-  val TERM_INT = "terms_int"
+  val TERM_INT = "term_int"
   val INT_ITEM = "int_item"
   val ITEM_INT = "item_int"
   val ITEM_TERM = "item_term"
